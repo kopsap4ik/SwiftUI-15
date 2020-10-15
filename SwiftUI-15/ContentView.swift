2513d9257100baff03ac7871e8ae2e94aead5e73
Vasily Petuhov <vspetukhov@icloud.com>
2020-10-15 10:29:49 +0300

    Initial Commit

---
+++ SwiftUI-15/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-15
+//
+//  Created by Василий Петухов on 15.10.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-15/SwiftUI_15App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_15App.swift
+//  SwiftUI-15
+//
+//  Created by Василий Петухов on 15.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
